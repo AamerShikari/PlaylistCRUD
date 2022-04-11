10760afe5540302573081ad17907ad538bef6469
AamerShikari <shikariaamer@gmail.com>
2022-04-11 18:42:36 -0400

    Go back function on song show page, bug fixing with youtube api

--- controllers/song.js
+++ controllers/song.js
@@ -4,7 +4,7 @@ const { google } = require('googleapis')
 module.exports = {
     index,
     add,
-    new: newYT,
+    new: newSong,
     show,
     delete: deleteSong
 }
@@ -19,30 +19,24 @@ function add (req, res) {
     res.render('song/add')
 }
 
-function newYT (req, res){
-    console.log(req.params)
-    console.log(req.body)
+function newSong (req, res){
+    let search = req.body.name + " " + req.body.artist;
     google.youtube('v3').search.list({
         key: process.env.YOUTUBE_TOKEN,
         part: 'snippet',
-        q: req.body.name,
+        q: search,
         maxResults: 1, 
-        order: "viewCount",
     }).then((response) => {
         const { data } = response;
         req.body.videoId = data.items[0].id.videoId
         console.log(req.body)
         const song = new Song(req.body)
-        console.log(song)
         song.save(function(err){
             res.redirect('/song/allSongs')
         })
     }).catch((err) => console.log(err));
 }
 
-function newSong (req, res) {
-    
-}
 
 function show (req, res) {
     Song.findOne({_id: req.params.id}).populate('reviews').exec(function(err,song){
